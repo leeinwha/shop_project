9aef582d0b97f1f2a228e340ad23828dac3beadd
iNa <aljard@naver.com>
2024-04-03 19:32:34 +0900

    2024/04/02 shopping mall project (product card component)

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,12 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
-
+import { useState } from 'react';
+import data from './data';
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">      
       <Navbar bg="light" data-bs-theme="light">
@@ -18,27 +22,29 @@ function App() {
       <div className='main-bg'></div>
 
       <div className='container'>
-        <div className='row'>
-          <div className='col-md-4'>
-            <img src={process.env.PUBLIC_URL + '/shoes1.jpg'} width='80%'/>
-            <h4></h4>
-            <p></p>
-          </div>
-          <div className='col-md-4'>
-            <img src={process.env.PUBLIC_URL + '/shoes2.jpg'} width='80%'/>
-            <h4></h4>
-            <p></p>
-          </div>
-          <div className='col-md-4'>
-            <img src={process.env.PUBLIC_URL + '/shoes3.jpg'} width='80%'/>
-            <h4></h4>
-            <p></p>
-          </div>
+        <div className='row'>          
+          {
+            shoes.map((a, i)=>{
+              return(
+                <Card shoes={shoes[i]} i={i}></Card>
+              )             
+            })
+          }          
         </div>
       </div>
 
     </div>
   );
+}
+
+function Card(props){
+  return(
+    <div className='col-md-4'>
+      <img src={process.env.PUBLIC_URL + '/shoes'+ (props.i+1) +'.jpg'} width='80%'/>
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
